8cf7fc5c88b1114bcb21ae07a9ba70e6759d810f
hallojenny <32244748+hallojenny@users.noreply.github.com>
2018-11-09 14:31:51 +0700

    Update and rename front-end to front.php

---
+++ front.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+  <!-- Import ไฟล์ jQuery -->
+<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+<script src="jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<title> front-end </title>
+
+
+
+</html>
